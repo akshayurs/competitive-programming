a48573a457d8bba8098f0363223326e0f353a70b
Akshay Urs <84136865+akshayurs@users.noreply.github.com>
2022-09-05 13:54:59 +0530

    bug fix

--- easy/kth_smallest.py
+++ easy/kth_smallest.py
@@ -1,4 +1,8 @@
 # O(klogn)
+# all test cases pass InterviewBit
+from array import array
+
+
 def heapify(arr, n, i):
     heap = False
     smallest = i
@@ -16,13 +20,15 @@ def heapify(arr, n, i):
 def kthsmallest(arr, n, k):
     for i in range(int(n/2)-1, -1, -1):
         heapify(arr, n, i)
-    for i in range(n-1, k-1, -1):
-        heapify(arr, i, 0)
-        arr[i], arr[0] = arr[0], arr[i]
+    n1 = n
+    for i in range(k):
+        n1 = n1-1
+        arr[n1], arr[0] = arr[0], arr[n1]
+        heapify(arr, n1, 0)
     return arr[n-k]
 
 
-arr = [6, 2, 4, 1, 5, 7]
+arr = [7, 10, 4, 20, 15]
 n = len(arr)
-k = 3
-print(kthsmallest(arr, n, k))  # 4
+k = 4
+print(kthsmallest(arr, n, k))  # 15
